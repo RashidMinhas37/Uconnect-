823e2215a4406f8ef57ea55c49677015a05d8f42
RashidMinhas <99872792+RashidMinhas37@users.noreply.github.com>
2022-05-09 09:42:56 -0400

    complete update

--- Switch statment/custom.js
+++ Switch statment/custom.js
@@ -45,57 +45,11 @@ switch(new Date().getTime()){
   case 6:
      day = 'saturday';
     break;
+    default :
+    console.log('invalid Day!');
+    break;
               
 }
 console.log(`To day is ${day}`);
 
 
-
-
-//incomplete
-
-let hours;
-
-switch(new Date().getDay()){
-      case 0:
-      hours = '00';
-      break;
-      case 1:
-      hours = '1';
-      break;
-      case 2:
-      hours = '2';
-      break;
-      case 3:
-      hours = '3';
-      break;
-      case 4:
-      hours = '4';
-      break;
-      case 5:
-      hours = '5';
-      break;
-      case 6:
-      hours = '6';
-      break;
-      case 7:
-      hours = '7';
-      break;
-      case 8:
-      hours = '8';
-      break;
-      case 9:
-      hours = '9';
-      break;
-      case 10:
-      hours = '10';
-      break;
-      case 11:
-      hours = '11';
-      break;
-      case 12:
-      hours = '12';
-      break;
-
-      };
-console.log(`the time is ${hours}`)
